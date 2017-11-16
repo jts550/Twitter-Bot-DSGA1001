9fd339d4fd83fc3eec5d8036075c194224a06aa2
Stephen Carrow <stephencarrow@Stephens-MacBook-Pro.local>
2017-11-15 21:12:16 -0500

    Sentiment on api dump; Main script pipelines and grid search

--- src/extract_sentiment_features.py
+++ src/extract_sentiment_features.py
@@ -14,42 +14,18 @@ import sys
 
 def main():
     
-    # Load honeypot data and label
+    # Load api dump data
     cwd = os.getcwd()
     main_directory = '/'.join(cwd.split('/')[:-1])
-    
-    file_list = glob.glob(main_directory + '/data/social_honeypot_icwsm_2011/' + '*.txt')
-    
-    cols_dict = {"content_polluters.txt": ["UserID", "CreatedAt", "CollectedAt", "NumerOfFollowings", 
-                                           "NumberOfFollowers", "NumberOfTweets", "LengthOfScreenName",
-                                           "LengthOfDescriptionInUserProfile"],
-                 "content_polluters_followings.txt": ["UserID", "SeriesOfNumberOfFollowings"],
-                 "content_polluters_tweets.txt": ["UserID", "TweetID", "Tweet", "CreatedAt"],
-                 "legitimate_users.txt": ["UserID", "CreatedAt", "CollectedAt", "NumerOfFollowings", 
-                                           "NumberOfFollowers", "NumberOfTweets", "LengthOfScreenName",
-                                           "LengthOfDescriptionInUserProfile"],
-                 "legitimate_users_followings.txt": ["UserID", "SeriesOfNumberOfFollowings"],
-                 "legitimate_users_tweets.txt": ["UserID", "TweetID", "Tweet", "CreatedAt"]}
-    
-    glb = globals()
-    for each_location in file_list:
-        each_file = each_location.split('/')[-1].split('.')[0]
-        glb[each_file+"_df"] = pd.read_table(each_location,
-           names=cols_dict[each_file+".txt"])
-        
-    content_polluters_tweets_df['label'] = 1
-    legitimate_users_tweets_df['label'] = 0
+
+    tweet_df = pd.read_csv(main_directory + '/data/varol_cleaned.csv')
     
     # Load ANEW dictionary
     anew_df = pd.read_csv(main_directory + '/data/Ratings_Warriner_et_al.csv',
                          header=0, index_col=0)
     anew_df.set_index('Word', inplace=True)
     
-    
-    #Apply sentiment feature extractors
-    tweet_df = pd.concat([legitimate_users_tweets_df,
-                          content_polluters_tweets_df], ignore_index=True)
-        
+    #Apply sentiment feature extractors        
     extractor = SentimentExtractor()
     tweets_vader = extractor.TweetsVADER(tweet_df.Tweet, tweet_df.TweetID.values)
     tweets_anew = extractor.TweetsANEW(tweet_df.Tweet, tweet_df.TweetID.values, anew_df)
@@ -78,7 +54,7 @@ def main():
             ).set_index('UserID'), left_index=True, right_index=True)
     
     # Save transformed to disk
-    sentiment_dist_df.to_csv(main_directory + '/data/sentiment_dist.csv')
+    sentiment_dist_df.to_csv(main_directory + '/data/sentiment_dist_varol_dump.csv')
     
 if __name__ == "__main__":
     
