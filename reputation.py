5cfdd3be68bde8e62498cd91c08ef5281e81f38e
chris <chris@darkcorner.net>
2017-11-17 14:33:22 -0500

    updates to script for computing taste

--- reputation.py
+++ reputation.py
@@ -1,66 +1,77 @@
-from twython import Twython
+#load libraries
+import tweepy
 import numpy as np
 import pandas as pd
 import os
+import json
+
+#login 
+APP_KEY = 'HOShGCrDyAdKRgudjn6wF1Zzg'
+APP_SECRET = 'IPCJVbKU5sJd85twNv6qgvRQXp4gFE5133bIprV76lBx3n5Jay'
+
+auth = tweepy.AppAuthHandler(APP_KEY, APP_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+
+lastPerc = 0
+
+#Get User Ids
+data_path = os.getcwd() + os.sep + "varol-2017-ids.csv"
+ids = pd.read_csv(data_path, usecols = [0] )
+twitUsers_incomplete = pd.DataFrame(columns = ["id", 'reputation', 'taste'])
+twitUsers_incomplete.id = ids
 
 
-screenNames = {'urbandaddy', 'nyudatascience', 'spacex'}
+#reputation
+def Reputation(user):
 
-USERLISTFILE = "TwitterUsers.csv"
-CWD = os.getcwd() + os.sep
+    rep = user.followers_count/(user.friends_count + user.followers_count)
 
-#replace these with your own app key and secret
-APP_KEY = '720258860880015361-eucto9mH525tPCqMFh7ZKHcADWY8Lkx'
-APP_SECRET = 'TpI9RJsHiqmUERIq9ISuwyZPjwFBtMfwYgkW09MRvKEIl'
-
-#update this to save it locally and only grab it the first time.
-twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
-ACCESS_TOKEN = twitter.obtain_access_token()
-
-twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-
-def userReputation(user):
-    #get friends and followers count
-    friends_count = [item['friends_count'] for item in user][0]
-    followers_count = [item['followers_count'] for item in user][0]    
-    rep = followers_count/(friends_count + followers_count)
-    
     return(rep)
 
-def userAccountTaste(sName):
-    
-    friends_nested = []
-    next_cursor = -1
-    
-    while(next_cursor != 0):
-        #Get page of friends
-        obj = (twitter.get_friends_list(screen_name=sName, count = 200, skip_status = True, cursor = next_cursor))
-        #Append to list
-        friends_nested.append(obj['users'])
-        #update cursor
-        next_cursor = obj['next_cursor']
-    
-    #flatten list
-    friend_list = [item for sublist in friends_nested for item in sublist]
-    
-    #Get friends and followers lists
-    friends_count = [item['friends_count'] for item in friend_list]
-    followers_count = [item['followers_count'] for item in friend_list]
-    taste = np.mean([fo/(fr + fo) for fr, fo in zip(friends_count, followers_count)])
-    
+#account taste
+def AccountTaste(user):
+    friends_rep = []
+    friendList = api.friends_ids(user_id = user.id)
+    print(" fl:", len(friendList), " ", end='')
+    for friend in friendList:
+        try:
+            user = api.get_user(user_id  = friend) 
+            friends_rep.append(Reputation(user))
+        except:
+           print("died on:", friend)
+    taste = np.nanmean(friends_rep)
+    #print("taste:", taste);
     return(taste)
 
-for sName in screenNames:
-    print("Scanning:", sName)
-
-    user  = twitter.lookup_user(screen_name=sName)    
-    reputation = userReputation(user)
-    account_taste = userAccountTaste(sName)    
-
-    temp = pd.DataFrame({'screen_name':[sName], 
-                         'reputation': [reputation], 
-                         'account_taste': [account_taste]})
-    twitUsers = twitUsers.append(temp)
+def doneYet(account):
+    global lastPerc
+    perc = np.round(twitUsers_incomplete.loc[twitUsers_incomplete['id']==account.id].index[0] / ids.shape[0], decimals = 2) * 100
+    if(perc > lastPerc ):
+        print("%d%% Done" %perc)
+        lastPerc = perc
 
 
+def FillAccount(account):
+    doneYet(account)
+    user=None
+    try:
+        print("processing:", account.id, end='')
+        user = api.get_user(user_id  = account.id)
+        account.reputation = Reputation(user)
+        account.taste = AccountTaste(user)
+        print("finished with:", user.id)
+    except tweepy.TweepError as e:
+        print("TweepError:",e)
+        account.reputation = np.nan
+        account.taste = np.nan
+        if (user != None):
+           print("bailed on :", user.id)
+
+    return(account)
+
+#twitUsers = twitUsers_incomplete.apply(FillAccount, ids, axis=1)
+twitUsers = twitUsers_incomplete.apply(FillAccount, axis=1)
+
+csv_path = os.getcwd() + os.sep + "reputation.csv"
+twitUsers.to_csv(csv_path, index = False)
 
