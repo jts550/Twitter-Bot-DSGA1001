cc27cad3a28c81a94959367248ba85058a7e57b5
Isaac Haberman <isaachaberman@gmail.com>
2017-11-26 17:29:48 -0500

    Added KNN, modified models.py

--- betweenCompare.py
+++ betweenCompare.py
@@ -7,7 +7,7 @@ from sklearn import metrics as mt
 def betweenCompare(models):
     fig, axes = plt.subplots(1,1, figsize=(8,6))
     for model in models:
-        fpr, tpr, thresholds = mt.roc_curve(model.y_test, model.best_cv.predictions[:,1])
+        fpr, tpr, thresholds = mt.roc_curve(model.y_test, model.best_model.predictions[:,1])
         roc_auc = mt.auc(fpr, tpr)
         axes.plot(fpr, tpr, label = model.name + " (AUC = {:0.3})".format(roc_auc))
     #plot aesthetics
--- model.py
+++ model.py
@@ -14,64 +14,56 @@ import matplotlib.pyplot as plt
 
 #Model class
 class Model:
-    names = []
-    predictions = None
-    pipeline = None 
-    cv_models = {}
-    best_cv = None
     
-    class CV_Model:
-        params = None
-        grid = None
-        grid_summary = None
-        model = None
-        predictions = None
+    class Iteration:
         
         def __init__(self, name):
             self.name = name
+            self.params = None
+            self.grid = None
+            self.grid_summary = None
+            self.model = None
+            self.predictions = None
     
     def __init__(self, name, model, X_train, y_train, X_test, y_test):
         #Name 
         self.name = name
-        self.names.append(name)
+        self.models = {}
+        self.best_model = None
         #Data            
         self.X_train = X_train
         self.y_train = y_train
         self.X_test = X_test
         self.y_test = y_test
+        self.baseline(model)
+        
+    def baseline(self, model):
         #Initialzie Baseline Model
-        self.model = model
-        self.fit = self.model.fit(X_train,y_train)
-        self.predict()
+        self.models["Baseline"] = self.Iteration("Baseline")
+        self.models["Baseline"].model = model
+        self.models["Baseline"].model.fit(self.X_train,self.y_train)
+        #SET PARAMS FOR BASELINE
+        self.predict("Baseline")
+        self.best_model = self.models["Baseline"]
         
-    def predict(self):
-        #baseline model predictions
-        self.predictions = self.model.predict_proba(self.X_test)
-        #auc
-        self.fpr, self.tpr, _ = mt.roc_curve(self.y_test, self.predictions[:,1])
-        self.auc = mt.auc(self.fpr, self.tpr)
-        
-    def cvPredict(self, name):
+    def predict(self, name):
         #cv models predictions
-        self.cv_models[name].predictions = self.cv_models[name].cv_model.predict_proba(self.X_test)
+        self.models[name].predictions = self.models[name].model.predict_proba(self.X_test)
         #auc
-        self.cv_models[name].fpr, self.cv_models[name].tpr, _ = mt.roc_curve(self.y_test, self.cv_models[name].predictions[:,1])
-        self.cv_models[name].auc = mt.auc(self.cv_models[name].fpr, self.cv_models[name].tpr)
+        self.models[name].fpr, self.models[name].tpr, _ = mt.roc_curve(self.y_test, self.models[name].predictions[:,1])
+        self.models[name].auc = mt.auc(self.models[name].fpr, self.models[name].tpr)
         
-    def bestCVModel(self, name):
+    def bestModel(self, name):
         #update best cv-model
-        #if none are present
-        if self.best_cv is None:
-            self.best_cv = self.cv_models[name]
         #only update if better
-        elif self.cv_models[name].auc > self.best_cv.auc: 
-            self.best_cv = self.cv_models[name]
+        if self.models[name].auc > self.best_model.auc: 
+            self.best_model = self.models[name]
             
     def plotLearnCurveMultiParam(self, name):
         #plot learning curve for two parameters
         fig, axes = plt.subplots(figsize=(8,6))
-        data = pd.pivot_table(self.cv_models[name].grid_summary, 
-                              index=self.cv_models[name].params[0], columns=self.cv_models[name].params[1])
+        data = pd.pivot_table(self.models[name].grid_summary, 
+                              index=self.models[name].params[0], columns=self.models[name].params[1])
         #Main line
         data['mean_score'].plot(ax=axes, figsize=(8,6))
         #std error lines
@@ -87,33 +79,33 @@ class Model:
         #Plot aesthetics
         plt.legend()
         plt.tight_layout()
-        plt.title("Learning Curve of {0}:\n{1}".format(self.cv_models[name].name, str(self.cv_models[name].params)))
+        plt.title("Learning Curve of {0}:\n{1}".format(self.models[name].name, str(self.models[name].params)))
         
     def plotLearnCurve(self, name):
         #plot learning curve for one parameter
         fig, axes = plt.subplots(figsize=(8,6))
         #Main Line
-        x_val = self.cv_models[name].grid_summary[self.cv_models[name].params]
-        max_idx = self.cv_models[name].grid_summary.mean_score.argmax()
-        axes.plot(x_val, self.cv_models[name].grid_summary.mean_score, 'C0', label='Mean AUC')
+        x_val = self.models[name].grid_summary[self.models[name].params]
+        max_idx = self.models[name].grid_summary.mean_score.argmax()
+        axes.plot(x_val, self.models[name].grid_summary.mean_score, 'C0', label='Mean AUC')
         #Std Error lines
-        lower = self.cv_models[name].grid_summary.mean_score - self.cv_models[name].grid_summary.std_err
+        lower = self.models[name].grid_summary.mean_score - self.models[name].grid_summary.std_err
         axes.plot(x_val, lower, 'C0', label='-1 Std.Err', linestyle='--')
-        upper = self.cv_models[name].grid_summary.mean_score + self.cv_models[name].grid_summary.std_err
+        upper = self.models[name].grid_summary.mean_score + self.models[name].grid_summary.std_err
         axes.plot(x_val, upper, 'C0', label='+1 Std.Err', linestyle='--')
         #1 std error rule
-        error_rule = self.cv_models[name].grid_summary.mean_score.max() - self.cv_models[name].grid_summary.std_err[max_idx]
+        error_rule = self.models[name].grid_summary.mean_score.max() - self.models[name].grid_summary.std_err[max_idx]
         xmin = x_val.min()
         xmax = x_val.max()
         plt.hlines(xmin = xmin, xmax = xmax, y = error_rule, color = 'r')
         #Plot aesthetics
         plt.legend()
         plt.tight_layout()
-        plt.title("Learning Curve of {0}:\n{1}".format(self.cv_models[name].name, str(self.cv_models[name].params)))
+        plt.title("Learning Curve of {0}:\n{1}".format(self.models[name].name, str(self.models[name].params)))
 
     def gridSearchSummary(self, name):
         #get data from gridsearch and return in better format
-        grid_summary = pd.DataFrame(self.cv_models[name].grid.cv_results_)
+        grid_summary = pd.DataFrame(self.models[name].grid.cv_results_)
         #iterate through parameters
         params_summary = defaultdict(list)
         for row in grid_summary.params:
@@ -123,33 +115,32 @@ class Model:
         #mean score
         params_summary_df['mean_score'] = grid_summary.mean_test_score
         #std error
-        scores_columns = ["split" + str(x)+ "_test_score" for x in range(0,self.cv_models[name].grid.cv)]
-        std_err = np.sqrt(grid_summary[scores_columns].var(axis = 1, ddof = 0)/self.cv_models[name].grid.cv)
+        scores_columns = ["split" + str(x)+ "_test_score" for x in range(0,self.models[name].grid.cv)]
+        std_err = np.sqrt(grid_summary[scores_columns].var(axis = 1, ddof = 0)/self.models[name].grid.cv)
         params_summary_df.insert(params_summary_df.columns.get_loc("mean_score")+1, 'std_err', std_err)
 
         return params_summary_df
 
-    def tuningIteration(self, name, estimator, param_grid, cv = 5, scoring = "roc_auc", plot = True):
+    def addIteration(self, name, estimator, param_grid, cv = 5, scoring = "roc_auc", plot = True):
         #BUild out gridsearch, cv model and plot learning curve
+        self.models[name] = self.Iteration(name)
         #Build pipeline 
-        self.cv_models[name] = self.CV_Model(name)
-        self.cv_models[name].pipeline = Pipeline([('variance_thresh', fs.VarianceThreshold()), ('estimator', estimator)])
+        self.models[name].pipeline = Pipeline([('variance_thresh', fs.VarianceThreshold()), ('estimator', estimator)])
         #Build CV grid then run on data
-        self.cv_models[name].grid = \
-            ms.GridSearchCV(self.cv_models[name].pipeline,param_grid = param_grid, cv = cv, scoring = scoring, n_jobs = 4)
-        self.cv_models[name].grid.fit(self.X_train, self.y_train)
+        self.models[name].grid = \
+            ms.GridSearchCV(self.models[name].pipeline,param_grid = param_grid, cv = cv, scoring = scoring, n_jobs = 4)
+        self.models[name].grid.fit(self.X_train, self.y_train)
         #print results
-        print("Best Score: {:0.6}\n".format(self.cv_models[name].grid.best_score_))
-        print("Best Params: ",self.cv_models[name].grid.best_params_)
+        print("Best Score: {:0.6}\n".format(self.models[name].grid.best_score_))
+        print("Best Params: ",self.models[name].grid.best_params_)
         #add summary and best model to Model
-        self.names.append(name)
-        self.cv_models[name].params = list(param_grid.keys())
-        self.cv_models[name].grid_summary = self.gridSearchSummary(name)
-        self.cv_models[name].cv_model = self.cv_models[name].grid.best_estimator_
-        self.cvPredict(name)
-        self.bestCVModel(name)
+        self.models[name].params = list(param_grid.keys())
+        self.models[name].grid_summary = self.gridSearchSummary(name)
+        self.models[name].model = self.models[name].grid.best_estimator_
+        self.predict(name)
+        self.bestModel(name)
         #plot learning curve if wanted
-        if len(self.cv_models[name].params) == 1 & plot:
+        if len(self.models[name].params) == 1 & plot:
             self.plotLearnCurve(name)
         elif plot:
             self.plotLearnCurveMultiParam(name)
@@ -157,16 +148,10 @@ class Model:
     def plotAUC(self, name):
         #Plot auc of model
         #check if cross-validated model
-        if name:
-            fpr = self.cv_models[name].fpr
-            tpr = self.cv_models[name].tpr
-            auc = self.cv_models[name].auc
-            title = "ROC Curve {}".format(self.cv_models[name].name)
-        else:
-            fpr = self.fpr
-            tpr = self.tpr
-            auc = self.auc
-            title = "ROC Curve {}".format(self.name)
+        fpr = self.models[name].fpr
+        tpr = self.models[name].tpr
+        auc = self.models[name].auc
+        title = "ROC Curve {}".format(name)
         #plot auc
         fig, axes = plt.subplots(1,1, figsize=(8,6))
         axes.plot(fpr, tpr, label = " (AUC = {:0.3})".format(roc_auc)) 
@@ -179,17 +164,10 @@ class Model:
     def withinCompare(self):
     #plot aucs of models
         fig, axes = plt.subplots(1,1, figsize=(8,6))
-        for name in self.names:
-            #If cv-model
-            if name in self.cv_models:
-                fpr = self.cv_models[name].fpr
-                tpr = self.cv_models[name].tpr
-                auc = self.cv_models[name].auc
-            #Baseline Model
-            else:
-                fpr = self.fpr
-                tpr = self.tpr
-                auc = self.auc
+        for name in self.models:
+            fpr = self.models[name].fpr
+            tpr = self.models[name].tpr
+            auc = self.models[name].auc
             axes.plot(fpr, tpr, label = name + " (AUC = {:0.3})".format(auc))
         #plot aesthetics
         plt.title("ROC Curves")
